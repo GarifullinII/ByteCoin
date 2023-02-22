36b2dcaf6b81c020d674f7c4d50f0f1ee178e32c
Ildar <ildaridrisovich@gmail.com>
2023-02-22 15:25:19 +0400

    add extension UIStackView

---
+++ ByteCoin/Controllers/ByteCoinViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Ildar Garifullin on 22/02/2023.
+//
+
+import UIKit
+
+class ByteCoinViewController: UIViewController {
+    
+    //MARK: - let/var
+    
+    
+    //MARK: - life cycle funcs
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+    //MARK: - flow funcs
+    
+    
+    //MARK: - public
+    
+    
+}
+
+//MARK: - extensions setupViews
+
+extension ByteCoinViewController {
+    
+    private func setupViews() {
+    }
+}
+
+//MARK: - extensions setConstraints
+
+extension ByteCoinViewController {
+    
+    private func setConstraints() {
+    }
+}
