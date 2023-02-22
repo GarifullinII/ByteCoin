887e9e848b18bf299cbbcbf9f444524e9b67c3a9
Ildar <ildaridrisovich@gmail.com>
2023-02-22 16:29:37 +0400

    add views and constraints

--- ByteCoin/Controllers/ByteCoinViewController.swift
+++ ByteCoin/Controllers/ByteCoinViewController.swift
@@ -11,6 +11,64 @@ class ByteCoinViewController: UIViewController {
     
     //MARK: - let/var
     
+    var byteCoinStackView = UIStackView()
+    
+    private let byteCoinLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ByteCoin"
+        label.textColor = UIColor(named: "Title Color")
+        label.font = UIFont.systemFont(ofSize: 50)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let coinView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private let byteCoinImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "bitcoinsign.circle.fill")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.text = "..."
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 25)
+        label.textAlignment = .right
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let currencyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "USD"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 25)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+//    private let currencyPickerView: UIPickerView = {
+//        let picker = UIPickerView()
+//        picker.translatesAutoresizingMaskIntoConstraints = false
+//
+//        return picker
+//    }()
     
     //MARK: - life cycle funcs
     
@@ -36,6 +94,16 @@ class ByteCoinViewController: UIViewController {
 extension ByteCoinViewController {
     
     private func setupViews() {
+        byteCoinStackView = UIStackView(
+            arrangedSubviews: [
+                byteCoinLabel,
+                coinView
+            ],
+            axis: .vertical,
+            spacing: 5,
+            distribution: .fillProportionally)
+        
+        view.addSubview(byteCoinStackView)
     }
 }
 
@@ -44,5 +112,15 @@ extension ByteCoinViewController {
 extension ByteCoinViewController {
     
     private func setConstraints() {
+        NSLayoutConstraint.activate([
+            byteCoinStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            byteCoinStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            byteCoinStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            byteCoinStackView.heightAnchor.constraint(equalToConstant: 165)
+        ])
+        
+        NSLayoutConstraint.activate([
+            coinView.heightAnchor.constraint(equalToConstant: 80)
+        ])
     }
 }
