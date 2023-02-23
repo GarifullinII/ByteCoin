5ee85fa65bdf8c34a49245deb2085130a71c38d6
Ildar <ildaridrisovich@gmail.com>
2023-02-24 00:07:46 +0400

    add request func

--- ByteCoin/Controllers/ByteCoinViewController.swift
+++ ByteCoin/Controllers/ByteCoinViewController.swift
@@ -118,6 +118,7 @@ extension ByteCoinViewController: UIPickerViewDelegate {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        print(coinManager.performRequest(urlString: "\("https://rest.coinapi.io/v1/exchangerate/BTC")/\("EUR")?apikey=\("???")"))
         return coinManager.getCoinPrice(for: coinManager.currencyArray[row])
     }
     
--- ByteCoin/Models/CoinManager.swift
+++ ByteCoin/Models/CoinManager.swift
@@ -10,27 +10,40 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "..."
+    let apiKey = "???"
     
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
-//    func parseJSON(_ data: Data) -> Double? {
-//        let decoder = JSONDecoder()
-//
-//        do {
-//            let decodedData = try decoder.decode(CoinData.self, from: data)
-//            let lastPrice = decodedData.last
-//
-//            return lastPrice
-//        } catch {
-//            delegate?.didFailWithError(self, error: error)
-//            return nil
-//        }
-//    }
-//
     func getCoinPrice(for currency: String) {
         let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+//        let urlString = "\("https://rest.coinapi.io/v1/exchangerate/BTC")/\("EUR")?apikey=\("???")"
         
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //create a URL
+        if let url = URL(string: urlString) {
+            //create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //give thesession a task
+            let task = session.dataTask(with: url) { data, response, error in
+                
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString!)
+                }
+                
+            }
+            
+            //start the task
+            task.resume()
+        }
     }
 }
